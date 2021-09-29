db4d9b496d453cd56981a6b66cc8a8e2d93d36b1
Victor Lee <vlee842@terpmail.umd.edu>
2021-09-29 01:10:40 -0400

    forgot to save a file

--- test/test_env.py
+++ test/test_env.py
@@ -1,4 +1,4 @@
-from environment_check import EnvironmentCheck
+from src.environment_check import EnvironmentCheck
 
 def test_mat_plot_vers():
     assert(EnvironmentCheck.checkMatPlotVers == "3.3.4")
