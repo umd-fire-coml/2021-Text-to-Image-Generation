ef0bf2bc805d73660979db2c9577000b29fe0557
Victor Lee <vlee842@terpmail.umd.edu>
2021-09-29 09:57:50 -0400

    updated libaries

--- test/test_env.py
+++ test/test_env.py
@@ -1,10 +1,10 @@
 from src.environment_check import EnvironmentCheck
 
 def test_mat_plot_vers():
-    assert(EnvironmentCheck.checkMatPlotVers() == '3.3.4')
+    assert(EnvironmentCheck.checkMatPlotVers() == '3.4.3')
 
 def test_pandas_vers():
-    assert(EnvironmentCheck.checkPandasVers() == '1.2.4')
+    assert(EnvironmentCheck.checkPandasVers() == '1.3.3')
 
 def test_np_vers():
-    assert(EnvironmentCheck.checkNPVers() == '1.20.1') 
+    assert(EnvironmentCheck.checkNPVers() == '1.21.2') 
