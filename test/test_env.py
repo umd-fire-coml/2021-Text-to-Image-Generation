6a90a0f682dd5f80dede9570d3cdf23feee26cd5
Victor Lee <vlee842@terpmail.umd.edu>
2021-10-04 11:34:44 -0400

    changed numpy version

--- test/test_env.py
+++ test/test_env.py
@@ -4,7 +4,7 @@ def test_mat_plot_vers():
     assert(EnvironmentCheck.checkMatPlotVers() == '3.3.4')
 
 def test_pandas_vers():
-    assert(EnvironmentCheck.checkPandasVers() == '1.2.4')
+    assert(EnvironmentCheck.checkPandasVers() == '1.3.3')
 
 def test_np_vers():
     assert(EnvironmentCheck.checkNPVers() == '1.21.2') 
