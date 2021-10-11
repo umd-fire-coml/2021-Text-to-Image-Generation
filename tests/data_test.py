d6b7522d0d11fa0b4db1a50a9003ac0d835aadff
unknown <justinhl@umd.edu>
2021-10-11 01:54:59 -0400

    better data verification test

--- tests/data_test.py
+++ tests/data_test.py
@@ -3,16 +3,12 @@ from os.path import exists
 
 import pytest
 
-@pytest.mark.parametrize("needed_files", [
+@pytest.mark.parametrize("needed_file", [
     "ind.mat",
     "language_original.mat",
     "subset_index.tar.gz"
 ])
 
-def test_data(needed_files):
-  all_files_available = True
-  for path in needed_files:
-    if not exists(os.path.join("data", path)):
-      all_files_available = False
-      break
-  assert(all_files_available)
+def test_data(needed_file):
+  print (os.path.join("data", needed_file))
+  assert(exists(os.path.join("data", needed_file)))
