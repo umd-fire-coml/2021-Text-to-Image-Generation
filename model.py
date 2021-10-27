64155de6dd9ec40164d9ebf018a1738fd02e92be
unknown <justinhl@umd.edu>
2021-10-27 12:09:21 -0400

    Added discriminator and respective tester

---
+++ model.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Reshape, Dropout, Dense 
+from tensorflow.keras.layers import Flatten, BatchNormalization
+from tensorflow.keras.models import Sequential, Model, load_model
+from tensorflow.keras.layers import LeakyReLU, UpSampling2D, Conv2D, Activation, ZeroPadding2D, concatenate
+
+image_shape = (256, 256, 3)
+text_encod_shape = (4, 4, 300)
+
+def build_discriminator():
+    input_layer = Input(shape=image_shape)
+
+    x = Conv2D(64, text_encod_shape[:2],
+               padding='same', strides=2,
+               input_shape=image_shape, use_bias=False)(input_layer)
+    x = LeakyReLU(alpha=0.2)(x)
+
+    x = Conv2D(128, text_encod_shape[:2], padding='same', strides=2, use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+
+    x = Conv2D(256, text_encod_shape[:2], padding='same', strides=4, use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+
+    x = Conv2D(512, text_encod_shape[:2], padding='same', strides=4, use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+
+    input_layer2 = Input(shape=text_encod_shape)
+
+    merged_input = concatenate([x, input_layer2])
+
+    x2 = Conv2D(64 * 8, kernel_size=1,
+                padding="same", strides=1)(merged_input)
+    x2 = BatchNormalization()(x2)
+    x2 = LeakyReLU(alpha=0.2)(x2)
+    x2 = Flatten()(x2)
+    x2 = Dense(1)(x2)
+    x2 = Activation('sigmoid')(x2)
+
+    discriminator = Model(inputs=[input_layer, input_layer2], outputs=[x2])
+    return discriminator
---
+++ tests/model_test.py
@@ -0,0 +1,22 @@
+import pytest
+from model import build_discriminator
+import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+import numpy as np
+import cv2
+
+def test_dis_output_shape():
+	dis = build_discriminator()
+	dis_optimizer = Adam(lr=0.1, beta_1=0.5, beta_2=0.999)
+	dis.compile(loss='binary_crossentropy', optimizer=dis_optimizer)
+	im = cv2.imread("Yoda.jpg")
+	prediction = dis([tf.expand_dims(im, 0),  tf.expand_dims(np.empty(shape=(4,4,300),dtype='int'), 0)])
+	assert(prediction.numpy().shape == (1,1))
+
+def test_dis_output_value():
+	dis = build_discriminator()
+	dis_optimizer = Adam(lr=0.1, beta_1=0.5, beta_2=0.999)
+	dis.compile(loss='binary_crossentropy', optimizer=dis_optimizer)
+	im = cv2.imread("Yoda.jpg")
+	prediction = dis([tf.expand_dims(im, 0),  tf.expand_dims(np.empty(shape=(4,4,300),dtype='int'), 0)])
+	assert(prediction.numpy()[0][0] >= 0 and prediction.eval()[0][0] <= 1)
