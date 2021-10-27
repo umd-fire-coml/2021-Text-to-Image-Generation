8951c6ecbd16b08ad3b0e10b9783557c5f3112d5
unknown <justinhl@umd.edu>
2021-10-27 12:18:54 -0400

    Added discriminator and respective tester

--- tests/model_test.py
+++ tests/model_test.py
@@ -19,4 +19,4 @@ def test_dis_output_value():
 	dis.compile(loss='binary_crossentropy', optimizer=dis_optimizer)
 	im = cv2.imread("Yoda.jpg")
 	prediction = dis([tf.expand_dims(im, 0),  tf.expand_dims(np.empty(shape=(4,4,300),dtype='int'), 0)])
-	assert(prediction.numpy()[0][0] >= 0 and prediction.eval()[0][0] <= 1)
+	assert(prediction.numpy()[0][0] >= 0 and prediction.numpy()[0][0] <= 1)
