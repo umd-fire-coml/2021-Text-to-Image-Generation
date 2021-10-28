60c3d98d0ffa16bd55700ff06199db8d16a1e676
unknown <justinhl@umd.edu>
2021-10-28 15:04:51 -0400

    update matplotlib version

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,7 +1,7 @@
 from src.environment_check import EnvironmentCheck
 
 def test_mat_plot_vers():
-    assert(EnvironmentCheck.checkMatPlotVers() == '3.3.4')
+    assert(EnvironmentCheck.checkMatPlotVers() == '3.4.3')
 
 def test_pandas_vers():
     assert(EnvironmentCheck.checkPandasVers() == '1.3.3')
