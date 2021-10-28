b2a57d992848e6642ed65b9ff9a602e554476d87
unknown <justinhl@umd.edu>
2021-10-28 14:58:34 -0400

    dead

--- tests/model_test.py
+++ tests/model_test.py
@@ -17,6 +17,6 @@ def test_dis_output_value():
 	dis = build_discriminator()
 	dis_optimizer = Adam(lr=0.1, beta_1=0.5, beta_2=0.999)
 	dis.compile(loss='binary_crossentropy', optimizer=dis_optimizer)
-	im = cv2.imread("Yoda.jpg")
+	im = image.imread("Yoda.jpg")
 	prediction = dis([tf.expand_dims(im, 0),  tf.expand_dims(np.empty(shape=(4,4,300),dtype='int'), 0)])
 	assert(prediction.numpy()[0][0] >= 0 and prediction.numpy()[0][0] <= 1)
