66d8c675500a344f1cf2cda897ddbaeeab3f256a
unknown <justinhl@umd.edu>
2021-10-28 14:56:11 -0400

    dead

--- tests/model_test.py
+++ tests/model_test.py
@@ -3,13 +3,13 @@ from model import build_discriminator
 import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
 import numpy as np
-import cv2
+from matplotlib import image
 
 def test_dis_output_shape():
 	dis = build_discriminator()
 	dis_optimizer = Adam(lr=0.1, beta_1=0.5, beta_2=0.999)
 	dis.compile(loss='binary_crossentropy', optimizer=dis_optimizer)
-	im = cv2.imread("Yoda.jpg")
+	im = image.imread("Yoda.jpg")
 	prediction = dis([tf.expand_dims(im, 0),  tf.expand_dims(np.empty(shape=(4,4,300),dtype='int'), 0)])
 	assert(prediction.numpy().shape == (1,1))
 
